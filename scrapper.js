38581091af911cba13228abcab57b930ba82ab67
tomika92 <tomika600@gmail.com>
2024-03-26 20:13:11 +0100

    final version

--- scrapper.js
+++ scrapper.js
@@ -11,6 +11,7 @@ const client = new Client({
     port: 5432
 })
 
+
 async function connentionWithDatabase(){
     try{
         await client.connect();
@@ -23,6 +24,8 @@ async function saveToDatabase(productsNames){
     try {
         const maxProductIdQuery = await client.query('SELECT MAX(products_id) AS maxProductId FROM products');
         let maxProductId = parseInt(maxProductIdQuery.rows[0].maxproductid || 1);
+        console.log(productsNames)
+
         for(const productName of productsNames){
             const product = await client.query('SELECT * FROM products WHERE product_name = $1', [productName]);
             if(product.rowCount === 0) {
@@ -41,39 +44,50 @@ async function fetch(url) {
     try {
         const response = await axios.get(url);
         const data = response.data;
+
         return data;
     } catch (err) {
         console.error('Do not found page', err);
+
         return null;
     }
 }
 
-function scrap(html, removedWords) {
+async function scrap(html, removedWords) {
     const $ = cheerio.load(html);
-    const regex = /\d/;
+
     const product = $('head title').text();
     const splitedProduct =  product.split('|');
-    const productTrim = splitedProduct[0].substring(0, splitedProduct[0].search(regex)).trim().toLowerCase();
+    const index = splitedProduct[0].search(/\d/);
+    //console.log(splitedProduct);
 
+    let productTrim =  splitedProduct[0].substring(0, index).trim().toLowerCase();
+    if (!productTrim) {
+        productTrim = splitedProduct[0].trim().toLowerCase();
+    }
+
+    console.log(productTrim);
     if(productTrim.includes('strona szczegółowa')){
         return null;
     }
-
-    const productName =  productTrim.replace(new RegExp(removedWords.map(r => r.source).join('|'), 'gi'), '').trim();
-    console.log(productName);
+  
+    const words = await removedWords;
+    const productName =  productTrim.replace(new RegExp(words.map(r => r.source).join('|'), 'gi'), '').trim();
 
     return productName;
 }
 
-function makeRegexes(){
+async function makeRegexes(){
     const removedWords = fs.readFileSync('removedWords.txt', 'utf-8')
     const removedWordsLines = removedWords.split('\n');
     const regexes = [];
+
     removedWordsLines.forEach(line => {
         const regexString = '^\\b(' + line.trim().split(' ').join('\\s+') + ')\\b';
         const regex = new RegExp(regexString, 'gi');
         regexes.push(regex);
     });
+
     return regexes;
 }
 
@@ -83,27 +97,33 @@ async function main() {
     let count = 0;
     await connentionWithDatabase();
 
-    const url = 'https://www.kaufland.pl/oferta/aktualny-tydzien/przeglad/strona-szczegolowa.so_id=20709200.html'
-    const html = await fetch(url)
-    const product = scrap(html, regexes);
-    if(product != null){
-        productsNames.push(product);
-        count += 1;
-        console.log(productsNames);
-        if(count >= 1){
-            await saveToDatabase(productsNames);
-            count = 0;
+    for (let i = 141050; i <= 148000; i++){
+        const paddedNumber = String(i).padStart(8, '0');
+        console.log(paddedNumber);
+
+        const url = `https://www.kaufland.pl/oferta/aktualny-tydzien/przeglad/strona-szczegolowa.so_id=${paddedNumber}.html`
+        //const url = 'https://www.kaufland.pl/oferta/aktualny-tydzien/przeglad/strona-szczegolowa.so_id=20091490.html'
+
+        const html = await fetch(url)
+        const product = scrap(html, regexes);
+        const resolvedProduct = await product;
+
+        if(resolvedProduct != null){
+            productsNames.push(resolvedProduct);
+
+            count += 1;
+            console.log(productsNames);
+            if(count >= 10){
+                await saveToDatabase(productsNames);
+                count = 0;
+                productsNames.splice(0, productsNames.length);
+            }
         }
     }
-    else {
-        console.log("do nothing");
+    if(count != 0) {
+        await saveToDatabase(productsNames);
     }
     await client.end();
-
-    /*for (let i = 1; i <= 99; i++) {
-        const paddedNumber = String(i).padStart(2, '0');
-        console.log(paddedNumber);
-    }*/
 }
 
 main()
